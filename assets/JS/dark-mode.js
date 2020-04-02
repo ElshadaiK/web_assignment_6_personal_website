9b0e797d7ad1a15975222c4d4350f9b4cff753fc
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2020-04-02 21:54:45 +0300

    Dark mode adjusted for all the pages

---
+++ assets/JS/dark-mode.js
@@ -0,0 +1,21 @@
+let theme = null;
+$(document).ready(function(){
+        check();
+        $('.js-dark-toggle').click(change);
+        
+    });
+let change =  function(){
+        $('.body-section').toggleClass('dark');
+        theme = (theme =='dark')? 'bright':'dark'
+        localStorage.setItem("theme", theme);
+        $('#dark-mode').toggleClass("fa-sun");
+    }
+let check = function() {
+  
+    if(localStorage.getItem("theme")){
+        let savedTheme = localStorage.getItem("theme");
+        if(savedTheme == "dark"){
+            theme = 'bright'
+            change();
+        }
+}}
