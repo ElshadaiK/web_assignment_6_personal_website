05cb7a57809911ce0961e2acab09da59da436a35
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2020-04-05 08:24:55 +0300

    Filtering feature added to collections page

--- assets/JS/filter.js
+++ assets/JS/filter.js
@@ -1,15 +1,12 @@
     $(document).ready(function(){
         $(".btn-lg").on("click", function() {
-          var value = $(this).attr('data-filter');
-          $(".collectionsList *").filter(function() {
-            //   console.log($(this))
-            // $(this).toggle($(this).hasClass(value))
-          
-          var filtered = $(this).filter(name => {
-            console.log(name);
-            name.hasClass(value)});
-            console.log(filtered + "filtered")
-            // $(this).toggle(classNames.indexOf(value) > -1);
-          });
-        });
+                    
+            let selector = $(this).attr('data-filter');
+            // $(".collectionsList *").not(selector).toggle();
+                $(".collectionsList .card").not(selector).hide();
+                $(".collectionsList *").filter(selector).show();
+            $(this).removeClass('active').addClass('active').siblings().removeClass('active all');
+            return false;
+
     });
+});
